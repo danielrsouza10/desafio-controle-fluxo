ce5339b0b62835739127c980a7a8352d2bdbfa76
Daniel Souza <danielrsouza10@gmail.com>
2023-07-07 09:12:30 -0300

    validando parametros com try/catch e exceptions

---
+++ controle-fluxo/src/Contador.java
@@ -0,0 +1,35 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Contador {
+    public static void main(String[] args){
+        Scanner contador = new Scanner(System.in).useLocale(Locale.US);
+
+        System.out.println("Digite o primeiro parametro: ");
+        int primeiroContador = contador.nextInt();
+
+        System.out.println("Digite o segundo parametro");
+        int segundoContador = contador.nextInt();
+
+        
+
+        try{
+            validarParametros(primeiroContador, segundoContador);
+             
+        } catch (ParametrosInvalidosException e){
+            System.out.println("O primeiro parametro deve ser maior do que o segundo");
+        }
+
+    }
+    static void validarParametros( int parametroUm, int parametroDois) throws ParametrosInvalidosException{
+        if(parametroUm < parametroDois){
+            throw new ParametrosInvalidosException();
+        }else{
+            int resultado = parametroUm - parametroDois;
+            for(int i = 1; i<= resultado; i++){
+                System.out.println("Imprimindo o número " + i);
+            }
+        }
+
+    }
+}
